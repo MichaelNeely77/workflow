5ba802e6e02a9ed1b506d640ed37b1ea0c3b8342
Michael Neely <michael@michaelneely.net>
2019-09-25 00:47:54 -0400

    Installing BrowserSync and creating Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,36 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const  browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+const SOURCEPATHS = {
+    
+    sassSource : 'src/scss/*.scss'
+}
+
+const APPPATH = {
+    root : 'app/',
+    css : 'app/css',
+    js : 'app/js'
+}
 
 gulp.task('sass', function() {
-    return gulp.src('src/scss/app.scss')
+    return gulp.src(SOURCEPATHS.sassSource)
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('app/css/app.css'));
+        .pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('default');
+gulp.task('serve', function() {
+    browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+        server: {
+            baseDir : APPPATH.root
+        }
+    })
+});
+
+gulp.task('default', gulp.series('sass', 'serve', function() {
+
+}));
+
+
+
